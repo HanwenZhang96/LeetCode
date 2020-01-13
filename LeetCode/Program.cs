f9606a5d5edee526f7dce6a6d9814b39dbb2c139
hzhang2 <hanwen.zhang@infor.com>
2020-01-13 14:13:41 +0800

    first commit

---
+++ LeetCode/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace LeetCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+
+        // 整数逆转
+        public static int Reverse(int x)
+        {
+            int res = 0;
+            try
+            {
+                while (x != 0)
+                {
+                    checked
+                    {
+                        res = res * 10 + x % 10;
+                    }
+                    x = x / 10;
+                }
+            }
+            catch (OverflowException e)
+            {
+                return 0;
+            }
+            return res;
+        }
+    }
+}
